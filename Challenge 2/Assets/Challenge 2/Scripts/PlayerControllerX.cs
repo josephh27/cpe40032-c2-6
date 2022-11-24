f215444cd62c917e0704cdbaf6d7474edb4d87d8
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2022-11-24 22:45:42 +0800

    Pushing the challenge 2 files into the remote repository.

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float time = 2.0f;
+    public float timer = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= time)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timer = 0;
+            }
+        }
+    }
+}
---
+++ Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int spawnInterval = Random.Range(3, 5);
+        //To spawn at the start at a random time
+        Invoke("SpawnRandomBall", spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        int spawnRange = Random.Range(0, ballPrefabs.Length);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[spawnRange], spawnPos, ballPrefabs[spawnRange].transform.rotation);
+
+        //Using recursion to change the spawning delay thus changing the spawning interval
+        int spawnInterval = Random.Range(3, 5);
+        Invoke("SpawnRandomBall", spawnInterval);
+
+    }
+
+}
